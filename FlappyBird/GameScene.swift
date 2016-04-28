58fb793b83d28908a9a3ece977cfcf6616142cb0
renoyin <renoyin9536@gmail.com>
2016-04-28 01:13:20 -0700

    pipes can move but positions need fix

--- FlappyBird/GameScene.swift
+++ FlappyBird/GameScene.swift
@@ -11,14 +11,19 @@ import SpriteKit
 class GameScene: SKScene {
     
     var bird = SKSpriteNode()
+    var pipeUpTexture = SKTexture()
+    var pipeDownTexture = SKTexture()
+    var PipeMoveAndRemove = SKAction()
+    let pipeGap = 150.0
+    
     
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         
-        print(String(self.frame.size.width))
-        print(String(self.frame.size.height))
-        print(String(self.size.width))
-        print(String(self.size.height))
+        //print(String(self.frame.size.width))
+        //print(String(self.frame.size.height))
+        //print(String(self.size.width))
+        //print(String(self.size.height))
         
         // Physics
         self.physicsWorld.gravity = CGVectorMake(0.0, -4.0)
@@ -36,7 +41,7 @@ class GameScene: SKScene {
         
         
         // ground physical body
-        var groundBody = SKNode()
+        let groundBody = SKNode()
         groundBody.position = CGPointMake(0, 0)
         groundBody.physicsBody = SKPhysicsBody(rectangleOfSize: CGSizeMake(self.frame.size.width, self.frame.size.height * 0.44))
         groundBody.physicsBody?.dynamic = false
@@ -56,7 +61,67 @@ class GameScene: SKScene {
         bird.physicsBody?.allowsRotation = false
         self.addChild(bird)
         
+        
+        
+        // pipe
+        pipeUpTexture = SKTexture(imageNamed: "pipeUp")
+        pipeDownTexture = SKTexture(imageNamed: "pipeDown")
+        
+        
+        
+        
+        let distanceToMove = CGFloat(self.frame.size.width + 2 * pipeDownTexture.size().width)
+        let movePipes = SKAction.moveByX(-distanceToMove, y: 0.0, duration: NSTimeInterval(3))
+        let removePipes = SKAction.removeFromParent()
+        PipeMoveAndRemove = SKAction.sequence([movePipes, removePipes])
+        
+        
+        
+        
+        
+        // spawn pipes
+    
+        let spawn = SKAction.runBlock({() in self.spawnPipes()})
+        let delay = SKAction.waitForDuration(NSTimeInterval(2.0))
+        let spawnThenDelay = SKAction.sequence([spawn, delay])
+        let spawnThenDelayForever = SKAction.repeatActionForever(spawnThenDelay)
+        
+        self.runAction(spawnThenDelayForever)
+
     }
+    
+    
+    
+    func spawnPipes() {
+        
+        let pipePair = SKNode()
+        pipePair.position = CGPointMake(self.frame.size.width + pipeUpTexture.size().width, 0)
+        //pipePair.zPosition = -10
+        
+        let height = UInt32(self.frame.size.height / 4)
+        //let y = arc4random() % height + height
+        
+        let pipeDown = SKSpriteNode(texture: pipeDownTexture)
+        pipeDown.setScale(1)
+        pipeDown.position = CGPointMake(0.0, pipeDown.size.height + CGFloat(pipeGap))
+        
+        pipeDown.physicsBody = SKPhysicsBody(rectangleOfSize: pipeDown.size)
+        pipeDown.physicsBody?.dynamic = false
+        pipePair.addChild(pipeDown)
+        
+        let pipeUp = SKSpriteNode(texture: pipeUpTexture)
+        pipeUp.setScale(1)
+        pipeUp.position = CGPointMake(0.0, self.frame.size.height)
+        
+        pipeUp.physicsBody = SKPhysicsBody(rectangleOfSize: pipeUp.size)
+        pipeUp.physicsBody?.dynamic = false
+        pipePair.addChild(pipeUp)
+        
+        pipePair.runAction(PipeMoveAndRemove)
+        self.addChild(pipePair)
+    }
+
+    
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
